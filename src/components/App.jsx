aff15f77eeda8d820ad2c37c7947da0d8d85b124
LongMingWei <131566573+LongMingWei@users.noreply.github.com>
2023-11-13 15:54:22 +0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -93,7 +93,7 @@ function App2() {
   return (
     <div className="App">
       <h1>Counter</h1>
-      <h2 style={{ color }}>{count} {pause}</h2>
+      <h2 style={{ color }}>{count}\n{pause}</h2>
       <div className='buttons'>
         <button onClick={Plus}>Count Up</button>
         <button onClick={Minus}>Count Down</button>
